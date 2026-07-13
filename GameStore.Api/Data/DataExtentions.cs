567764430b32c71c29695907462ae379d65d0b23
Wyandhanu Maulidan Nugraha <wyandhanupapoy@gmail.com>
2026-07-13 21:40:20 +0700

    feat(db): add genre seed data and extract database configuration into extension method

--- GameStore.Api/Data/DataExtentions.cs
+++ GameStore.Api/Data/DataExtentions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -9,5 +10,28 @@ public static class DataExtentions
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "DataSource=GameStore.db";
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Platformer" },
+                        new Genre { Name = "Racing" },
+                        new Genre { Name = "Sports" }
+                    );
+
+                    context.SaveChanges();
+                }
+            })
+        );
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,13 +1,12 @@
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
+using GameStore.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
-
-var connString = "DataSource=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.AddGameStoreDb();
 
 var app = builder.Build();
 
