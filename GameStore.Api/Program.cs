5256079aa01a95304a3e26d4037584bae96d31e7
Wyandhanu Maulidan Nugraha <wyandhanupapoy@gmail.com>
2026-07-13 16:56:01 +0700

    feat(games): add GET endpoints for retrieving all games and game by id

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,34 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy VII Rebirth",
+        "RPG",
+        69.99M,
+        new DateOnly(2024, 2, 29)),
+    new (
+        3,
+        "Astro Bot",
+        "Platformer",
+        59.99M,
+        new DateOnly(2024, 9, 6)),
+];
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/1
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
