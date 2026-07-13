d117b005b28d2bffb332d99c7b118dc666c8fb1e
Wyandhanu Maulidan Nugraha <wyandhanupapoy@gmail.com>
2026-07-13 21:20:26 +0700

    feat(db): add initial EF Core migration and auto-migrate on startup

---
+++ GameStore.Api/Data/DataExtentions.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtentions
+{
+    public static void MigrateDb(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -12,4 +12,7 @@ builder.Services.AddSqlite<GameStoreContext>(connString);
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
+
 app.Run();
